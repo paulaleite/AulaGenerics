9d9f5188052e90f2bfdc1b645c14cf976cd2fe6a
Paula Leite <paula.t.leite@icloud.com>
2020-04-27 17:01:46 -0300

    working

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -18,6 +18,21 @@ pilha.push(intA)
 pilha.push(intB)
 
 //TODO: Fazer um método que recebe um Array e um elemento, retorna um Bool dizendo se o elemento se encontra no Array
+func inList<Element: Comparable>(_ array: [Element], _ element: Element) -> Bool {
+    for i in array {
+        if i == element {
+            return true
+        }
+    }
+    return false
+}
+
+let array = [1, 2, 3, 4]
+let elem = 4
+let elem2 = 5
+inList(array, elem)
+inList(array, elem2)
+
 //TODO: Extensão de Array contendo uma função que retorna a soma de todos os elementos, independente de qual o valor numérico
 //TODO: Fazer um método que recebe 2 Arrays e retorna um Int referente a quantidade de elementos presentes em ambos os Arrays
     //BONUS TODO: Fazer o método acima receber N arrays
